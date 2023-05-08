5826cfa93c62a3588a176adb07b032a43e98aa50
osawa-koki <github@osawa.dev>
2023-05-08 20:31:10 +0900

    請求金額の取得方法変えた。

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,23 @@
 import AWS from "aws-sdk";
+import dayjs from "dayjs";
+
+type CostExplorer = {
+  ResultsByTime: {
+    TimePeriod: {
+      Start: string;
+      End: string;
+    };
+    Total: {
+      UnblendedCost: {
+        Amount: string;
+        Unit: string;
+      };
+    };
+    Groups: [];
+    Estimated: boolean;
+  }[];
+  DimensionValueAttributes: [];
+};
 
 AWS.config.getCredentials(function(err) {
   if (err) console.error(err.stack);
@@ -17,14 +36,27 @@ const getCostExplorer = async () => {
   const costexplorer = new AWS.CostExplorer();
   const params = {
     TimePeriod: {
-      Start: "2023-01-01",
-      End: "2023-05-01",
+      Start: dayjs().subtract(1, "month").format("YYYY-MM-DD"),
+      End: dayjs().format("YYYY-MM-DD"),
     },
     Granularity: "MONTHLY",
     Metrics: ["UnblendedCost"],
   };
   const result = await costexplorer.getCostAndUsage(params).promise();
-  console.log(result);
+
+  if ((result?.ResultsByTime?.length || 0) === 0) {
+    console.error("No data.");
+    return;
+  }
+
+  // 今月の請求金額を取得
+  const start = result?.ResultsByTime?.slice(-1)[0]?.TimePeriod?.Start;
+  const end = result?.ResultsByTime?.slice(-1)[0]?.TimePeriod?.End;
+  const cost = result?.ResultsByTime?.slice(-1)[0]?.Total?.UnblendedCost?.Amount;
+  const costUnit = result?.ResultsByTime?.slice(-1)[0]?.Total?.UnblendedCost?.Unit;
+
+  console.log(`請求期間: ${start} ~ ${end}`);
+  console.log(`請求金額: ${cost} ${costUnit}`);
 }
 
 getCostExplorer();
