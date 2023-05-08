c8a75988a560195e63351564f5462e1536153c4b
osawa-koki <github@osawa.dev>
2023-05-08 20:33:13 +0900

    UnblendedCost -> BlendedCost

--- src/index.ts
+++ src/index.ts
@@ -40,7 +40,7 @@ const getCostExplorer = async () => {
       End: dayjs().format("YYYY-MM-DD"),
     },
     Granularity: "MONTHLY",
-    Metrics: ["UnblendedCost"],
+    Metrics: ["BlendedCost", "UnblendedCost"],
   };
   const result = await costexplorer.getCostAndUsage(params).promise();
 
@@ -52,8 +52,8 @@ const getCostExplorer = async () => {
   // 今月の請求金額を取得
   const start = result?.ResultsByTime?.slice(-1)[0]?.TimePeriod?.Start;
   const end = result?.ResultsByTime?.slice(-1)[0]?.TimePeriod?.End;
-  const cost = result?.ResultsByTime?.slice(-1)[0]?.Total?.UnblendedCost?.Amount;
-  const costUnit = result?.ResultsByTime?.slice(-1)[0]?.Total?.UnblendedCost?.Unit;
+  const cost = result?.ResultsByTime?.slice(-1)[0]?.Total?.BlendedCost?.Amount;
+  const costUnit = result?.ResultsByTime?.slice(-1)[0]?.Total?.BlendedCost?.Unit;
 
   console.log(`請求期間: ${start} ~ ${end}`);
   console.log(`請求金額: ${cost} ${costUnit}`);
