9c432f904701e08cea73ac9d93644ed7b477b6f6
osawa-koki <github@osawa.dev>
2023-05-08 20:36:38 +0900

    リファクタリング。

--- src/index.ts
+++ src/index.ts
@@ -1,62 +1,16 @@
-import AWS from "aws-sdk";
-import dayjs from "dayjs";
+import getCostExplorer from "./CostExplorer";
 
-type CostExplorer = {
-  ResultsByTime: {
-    TimePeriod: {
-      Start: string;
-      End: string;
-    };
-    Total: {
-      UnblendedCost: {
-        Amount: string;
-        Unit: string;
-      };
-    };
-    Groups: [];
-    Estimated: boolean;
-  }[];
-  DimensionValueAttributes: [];
-};
+getCostExplorer()
+  .then((result) => {
+    // 今月の請求金額を取得
+    const start = result?.ResultsByTime?.slice(-1)[0]?.TimePeriod?.Start;
+    const end = result?.ResultsByTime?.slice(-1)[0]?.TimePeriod?.End;
+    const cost = result?.ResultsByTime?.slice(-1)[0]?.Total?.BlendedCost?.Amount;
+    const costUnit = result?.ResultsByTime?.slice(-1)[0]?.Total?.BlendedCost?.Unit;
 
-AWS.config.getCredentials(function(err) {
-  if (err) console.error(err.stack);
-  else {
-    console.log("Access key:", AWS.config.credentials?.accessKeyId);
-  }
-});
-
-// リージョンを設定
-AWS.config.update({ region: "us-east-1" });
-
-console.log("Region: ", AWS.config.region);
-
-// 請求金額を取得
-const getCostExplorer = async () => {
-  const costexplorer = new AWS.CostExplorer();
-  const params = {
-    TimePeriod: {
-      Start: dayjs().subtract(1, "month").format("YYYY-MM-DD"),
-      End: dayjs().format("YYYY-MM-DD"),
-    },
-    Granularity: "MONTHLY",
-    Metrics: ["BlendedCost", "UnblendedCost"],
-  };
-  const result = await costexplorer.getCostAndUsage(params).promise();
-
-  if ((result?.ResultsByTime?.length || 0) === 0) {
-    console.error("No data.");
-    return;
-  }
-
-  // 今月の請求金額を取得
-  const start = result?.ResultsByTime?.slice(-1)[0]?.TimePeriod?.Start;
-  const end = result?.ResultsByTime?.slice(-1)[0]?.TimePeriod?.End;
-  const cost = result?.ResultsByTime?.slice(-1)[0]?.Total?.BlendedCost?.Amount;
-  const costUnit = result?.ResultsByTime?.slice(-1)[0]?.Total?.BlendedCost?.Unit;
-
-  console.log(`請求期間: ${start} ~ ${end}`);
-  console.log(`請求金額: ${cost} ${costUnit}`);
-}
-
-getCostExplorer();
+    console.log(`請求期間: ${start} ~ ${end}`);
+    console.log(`請求金額: ${cost} ${costUnit}`);
+  })
+  .catch((err) => {
+    console.error(err);
+  })
