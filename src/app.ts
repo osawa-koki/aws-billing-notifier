7eb70d652fcabc951dc25e7d76db2d667c868016
osawa-koki <github@osawa.dev>
2023-05-08 20:59:14 +0900

    Lambdaに登録した。

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,36 @@
 /* eslint-disable @typescript-eslint/require-await */
 
+import GetCostExplorer from "./GetCostExplorer";
+import SlackNotifier from "./SlackNotifier";
+
 export const lambdaHandler = async () => {
   try {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: 'hello world',
-      }),
-    }
+    GetCostExplorer()
+    .then((result) => {
+      // 今月の請求金額を取得
+      const start = result?.ResultsByTime?.slice(-1)[0]?.TimePeriod?.Start;
+      const end = result?.ResultsByTime?.slice(-1)[0]?.TimePeriod?.End;
+      const cost = result?.ResultsByTime?.slice(-1)[0]?.Total?.BlendedCost?.Amount;
+      const costUnit = result?.ResultsByTime?.slice(-1)[0]?.Total?.BlendedCost?.Unit;
+
+      console.log(`請求期間: ${start} ~ ${end}`);
+      console.log(`請求金額: ${cost} ${costUnit}`);
+
+      // Slackに通知
+      SlackNotifier([
+        `🐙🐙🐙🐙🐙 今月のAWS請求金額 🐙🐙🐙🐙🐙`,
+        ``,
+        `💰💰💰 請求期間: ${start} ~ ${end}`,
+        `💰💰💰 請求金額: ${cost} ${costUnit}`,
+        ``,
+        `🦑🐸🐬🐪🦈🐄🦒🐉🦁🐔🦀🐍🐶😻🐋🐗🐡`
+      ].join('\n'))
+    })
+    .catch((err: unknown) => {
+      console.error(err);
+    })
   } catch (err) {
-    console.log(err)
+    console.error(`[ERROR]: ${err}`)
     return err
   }
 }
